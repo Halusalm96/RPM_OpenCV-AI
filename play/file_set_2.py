2be47913295d1581cd9a52256e18f0b876836578
Halusalm96 <dmadkr6004@naver.com>
2024-06-11 09:58:37 +0900

    YOLOV8 학습 완료

---
+++ play/file_set_2.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+# import matplotlib.pyplot as plt
+# import cv2
+# import torch
+
+# YOLOv8 모델 로드
+model = YOLO('yolov8n.pt')
+
+model.train(data='/Users/503/play/coco8.yaml',epochs=10, patience=10, batch=32, imgsz=416)
+
+# 데이터셋 로드
+# dataset = model.load_data(data='/Users/503/play/PLAY.yaml',epochs=15, patience=10, batch=32, imgsz=416)
+
+# 모델 학습
+# model.train(data=dataset, epochs=20)
+
+# # 데이터셋의 첫 번째 샘플 확인
+# for img, label in dataset:
+#     img = cv2.cvtColor(img.numpy(), cv2.COLOR_BGR2RGB)
+#     plt.imshow(img)
+#     plt.show()
+#     print(label)
+#     break
+
+# 학습된 클래스 이름 출력
+print(type(model.names), len(model.names))
+print(model.names)
---
+++ play/test.py
@@ -0,0 +1,41 @@
+import cv2
+from ultralytics import YOLO
+
+def run_webcam():
+    # 학습된 모델을 로드합니다
+    model = YOLO('/Users/503/play/runs/detect/train/weights/last.pt')  # 학습된 모델 파일의 경로를 지정합니다
+    
+    # 웹캠 연결 (0은 기본 카메라를 의미합니다)
+    cap = cv2.VideoCapture(0)
+    
+    if not cap.isOpened():
+        print("오류: 웹캠을 열 수 없습니다.")
+        return
+
+    while True:
+        # 프레임을 하나씩 캡처합니다
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("오류: 프레임을 읽을 수 없습니다.")
+            break
+
+        # YOLO 모델을 사용하여 프레임을 처리합니다
+        results = model(frame)
+
+        # 결과를 프레임에 그립니다
+        annotated_frame = results[0].plot()  # plot()은 주석이 달린 프레임을 반환합니다
+        
+        # 결과 프레임을 표시합니다
+        cv2.imshow('YOLOv8 Detection', annotated_frame)
+        
+        # 'q' 키를 누르면 루프를 종료합니다
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # 웹캠을 해제하고 모든 창을 닫습니다
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    run_webcam()
